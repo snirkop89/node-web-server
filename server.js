1d6fdadad970ef0c36f8d56d2503bfc77adab9ee
Snir Koppelman <snirkwork@gmail.com>
2018-09-11 17:33:24 +0300

    Projects page added

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Portfolio'
+    });
+});
+
 // /bad, send json with error message prop
 app.get('/bad', (req, res) => {
     res.send({
