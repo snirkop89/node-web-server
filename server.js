912f2b938270a3a141f0553a4a9f84fd5c319918
Snir Koppelman <snirkwork@gmail.com>
2018-09-11 16:48:31 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to apeend to server.log.')
+        }
+    });
+
+    // oNly after next the app wil continue to run
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintanence.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome beautiful user to my site'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+// /bad, send json with error message prop
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to Connect'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
